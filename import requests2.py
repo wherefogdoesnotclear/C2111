2177ac9326207ef947a5cec3b69663060890b504
Mari <marichajk2020@gmail.com>
2025-05-16 21:57:18 +0300

    Add files via upload

---
+++ import requests2.py
@@ -0,0 +1,15 @@
+import tkinter
+def first_function():
+    pass
+class Human:
+    pass
+tk = tkinter
+first_f = first_function
+nick = Human
+print(tkinter.__name__)
+print(tk.__name__)
+print(first_function.__name__)
+print(first_f.__name__)
+print(Human.__name__)
+print(nick.__name__)
+print(__name__)
